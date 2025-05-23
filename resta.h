6e3820891b5376c01ca5d462bcc46fd4e7426cf0
DiegoRoca05 <132920879+DiegoRoca05@users.noreply.github.com>
2025-05-22 20:24:20 -0600

    Create resta.h

---
+++ resta.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2025 Tec de Monterrey
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef RESTA_H
+#define RESTA_H
+
+#include "operacion.h"
+
+class Resta : public Operacion {
+public:
+    double calcular(double x, double y) const override {
+        return x - y;
+    }
+};
+
+#endif // RESTA_H
